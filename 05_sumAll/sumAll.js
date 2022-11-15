f41fa8868b5aa4ececc6749d75ccba36d8746a53
Rachel Banana <117894827+RachelBanana03@users.noreply.github.com>
2022-11-15 14:57:33 +0800

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    if (typeof startNum != "number" || typeof endNum !="number" || startNum < 0 || endNum < 0) {
+        return "ERROR";
+    }
+    if (startNum > endNum) [startNum, endNum] = [endNum, startNum];
+    return (startNum+endNum)*(endNum-startNum+1)/2;
 };
 
 // Do not edit below this line
